ba7e678957da0832f32f90391a11015bb49d9164
sachin parashar <s.parashar2806@gmail.com>
2026-06-19 15:45:38 +0530

    chore: remove unnecessary test and debug scripts from repository

--- test_rename_flow.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def get_chats():
-    with open("chats.json", "r") as f:
-        return json.load(f)
-
-chats = get_chats()
-for k,v in chats.items():
-    print(f"{k}: {v['title']} - {len(v.get('messages', []))} messages")
